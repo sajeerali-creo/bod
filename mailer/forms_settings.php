d1f8b5ff9df36a04fd387961cdfbc4e7074705b2
Timelino Design <sajeer.ali09@gmail.com>
2025-07-29 18:06:36 +0400

    update

--- mailer/forms_settings.php
+++
@@ -1,8 +0,0 @@
-<?php
-	/* Forms settings */
-	$subj = "New message from 'Ruizarch'"; //letter subject
-	$to = 'yourname@domain.com'; // Enter Your E-mail
-	$from = 'admin@domain.com'; // Admin e-mail
-	$fromName = 'Ruizarch'; // Your company name
-	$charset = 'UTF-8';
-?>
